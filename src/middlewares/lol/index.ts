c9809aaafd2fed895a545c98b141c4ef9948a4b4
Kim jonghun <developerkimjh@gmail.com>
2023-01-24 14:31:17 +0900

    FEAT: userInfo 가져오는 api 생성
    api/lol/userInfo/:username

--- src/middlewares/lol/index.ts
+++ src/middlewares/lol/index.ts
@@ -3,6 +3,8 @@ import apiRequest from "../../utils/apiRequest";
 
 const maria = require("../../database/maria");
 
+const apiKey = process.env.RIOT_API_KEY;
+
 const getGameVersion = async (req: Request, res: Response) => {
   const response = await apiRequest<any>({
     url: "https://ddragon.leagueoflegends.com/api/versions.json",
@@ -11,8 +13,22 @@ const getGameVersion = async (req: Request, res: Response) => {
 
   res.send(response.data);
 };
+
+const getUserInfo = async (req: Request, res: Response) => {
+  const response = await apiRequest<any>({
+    url: `https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.username}`,
+    method: "get",
+    headers: {
+      "X-Riot-Token": apiKey,
+    },
+  });
+
+  res.send(response.data);
+};
+
 const Lol = {
   getGameVersion: [getGameVersion],
+  getUserInfo: [getUserInfo],
 };
 
 export default Lol;
--- src/routes/lol/index.ts
+++ src/routes/lol/index.ts
@@ -3,5 +3,6 @@ import Lol from "../../middlewares/lol/index";
 const Router = require("express").Router();
 
 Router.route("/version").get(Lol.getGameVersion);
+Router.route("/userInfo/:username").get(Lol.getUserInfo);
 
 module.exports = Router;
