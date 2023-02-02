0e34a5ee42551ccbdbafc0634673ec8cd202bd9a
Kim jonghun <developerkimjh@gmail.com>
2023-02-02 21:19:48 +0900

    FIX: api url 변경

--- src/routes/lol/index.ts
+++ src/routes/lol/index.ts
@@ -4,8 +4,8 @@ import Lol from "../../middlewares/lol/index";
 const Router = express.Router();
 
 Router.route("/version").get(Lol.getGameVersion);
-Router.route("/userInfo/:username").get(Lol.getUserInfo);
-Router.route("/userTier/:id").get(Lol.getUserTier);
-Router.route("/userGameRecord/:puuid").get(Lol.getUserGameRecord);
+Router.route("/info/:username").get(Lol.getUserInfo);
+Router.route("/tier/:id").get(Lol.getUserTier);
+Router.route("/record/:puuid").get(Lol.getUserGameRecord);
 
 export default Router;
